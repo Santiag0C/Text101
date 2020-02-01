a40dff6eac09a51196189ea9ff611ffa7b69efd4
Santiago Caraveo <santiagocbrvg@hotmail.com>
2020-01-31 17:33:36 -0700

    adds state script and the main adbentur game script

---
+++ AdbentureGame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdbentureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+
+    }
+
+    private void ManageState()
+    {
+        var nextState = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextState[0]
+        }
+    }
+}
